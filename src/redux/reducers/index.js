2adeec98e810e7b3c379f787dd0b78e3f2b404c0
luca <l.lobacher@web.de>
2020-02-12 21:26:39 +0100

    Refactors reducer to handle COUNTRIES_LOADED action type

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,18 @@
-const initialState = {}
+import { COUNTRIES_LOADED } from '../constants'
+
+const initialState = {
+  remoteCountries: []
+}
 
 const rootReducer = (state = initialState, action) => {
-  return state
+  switch (action.type) {
+    case COUNTRIES_LOADED:
+      return Object.assign({}, state, {
+        remoteCountries: state.remoteCountries.concat(action.payload)
+      })
+    default:
+      return state
+  }
 }
 
 export default rootReducer
