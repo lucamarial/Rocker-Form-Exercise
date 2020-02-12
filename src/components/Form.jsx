1ead9b8ca28c894c425258cace32b9024582ec1e
luca <l.lobacher@web.de>
2020-02-12 20:58:06 +0100

    Creates Form and FormFields component and renames App

---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import FormFields from './FormFields'
+
+class Form extends Component {
+  render() {
+    return (
+      <>
+        <FormFields />
+      </>
+    )
+  }
+}
+
+export default Form
---
+++ src/components/FormFields.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FormFields = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default FormFields
