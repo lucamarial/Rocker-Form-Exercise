b47a21bee3d0579d1b02cc900b288bbb23d78575
luca <l.lobacher@web.de>
2020-02-12 21:55:22 +0100

    Adds handleChange handler and value to input fields

--- src/components/FormFields.jsx
+++ src/components/FormFields.jsx
@@ -11,23 +11,35 @@ const FormFields = props => {
 						fluid
 						placeholder='Social Security Number (12 digits)'
 						type='text'
+						onChange={props.handleChange}
+            value={props.ssn}
+            noValidate
 					/>
 					<Form.Input
 						name='phone'
 						fluid
 						placeholder='Phone (+46)'
 						type='tel'
+						onChange={props.handleChange}
+            value={props.phone}
+            noValidate
 					/>
 					<Form.Input
 						name='email'
 						fluid
 						placeholder='Email'
 						type='email'
+						onChange={props.handleChange}
+            value={props.email}
+            noValidate
 					/>
 					<select
 						className='fluid field'
 						name='country'
 						placeholder='Country'
+						onChange={props.handleChange}
+            value={props.country}
+            noValidate
 					>
             {props.options}
 					</select>
