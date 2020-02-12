06e10617b5fa43ebb29390f5e87b5492117f5d88
luca <l.lobacher@web.de>
2020-02-12 21:06:51 +0100

    Installs semantic ui and adds Form and Container

--- src/components/FormFields.jsx
+++ src/components/FormFields.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
+import { Form, Container } from 'semantic-ui-react'
 
 const FormFields = () => {
-  return (
-    <>
-      
-    </>
-  )
+	return (
+		<>
+			<Container fluid textAlign='center'>
+				<Form>
+					<Form.Input
+						name='ssn'
+						fluid
+						placeholder='Social Security Number (12 digits)'
+						type='text'
+					/>
+					<Form.Input
+						name='phone'
+						fluid
+						placeholder='Phone (+46)'
+						type='tel'
+					/>
+					<Form.Input
+						name='email'
+						fluid
+						placeholder='Email'
+						type='email'
+					/>
+					<select
+						className='fluid field'
+						name='country'
+						placeholder='Country'
+					>
+					</select>
+					<Form.Button content='Confirm' type='submit' />
+				</Form>
+			</Container>
+		</>
+	)
 }
 
 export default FormFields
