ff16f1456f1dcdc8d82f6c2391eb57b74850b761
luca <l.lobacher@web.de>
2020-02-12 23:38:05 +0100

    Adds persistReducer and persistStore
    Adds functionality to redux store to persist remoteCountries state in
    session storage.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import 'semantic-ui-css/semantic.min.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import { Provider } from 'react-redux'
-import store from './redux/store'
+import { store } from './redux/store'
 
 ReactDOM.render(
 	<Provider store={store}>
