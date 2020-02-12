47477afe45bc5e422eaa4c69dcd94e45ec8dcaca
luca <l.lobacher@web.de>
2020-02-12 22:03:44 +0100

    Adds handleChange funtionality to validate form fields and to handle errors

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -20,6 +20,48 @@ class Form extends Component {
     this.props.getCountries()
   }
 
+  validEmail = RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i)
+
+  ssnRegex = /^(19|20)?\d{2}((0[1-9])|(1[012]))(([012][1-9])|(3[01]))-\d{4}$/
+  validSsn = ssn => this.ssnRegex.test(ssn)
+
+  phoneRegex = /^([+]46)\s*(7[0236])\s*(\d{4})\s*(\d{3})$/
+  validPhone = phone => this.phoneRegex.test(phone)
+
+  handleChange = (e) => {
+    e.preventDefault()
+    const { name, value } = e.target
+    let errors = this.state.errors
+
+    switch (name) {
+      case 'ssn':
+        errors.ssn = 
+          this.validSsn(value)
+            ? ''
+            : 'Please enter a valid personal number!'
+        break
+      case 'phone':
+        errors.phone =
+          this.validPhone(value)
+            ? ''
+            : 'Please enter a valid email address!'
+        break
+      case 'email':
+        errors.email =
+          this.validEmail.test(value)
+            ? ''
+            : 'Please enter a valid email address!'
+        break
+      default:
+        break
+    }
+
+    this.setState({
+      [name]: value,
+      errors, [name]: value
+    })
+  }
+
   render() {
     let { remoteCountries } = this.props
     let options
@@ -40,6 +82,7 @@ class Form extends Component {
           phone = {this.state.phone}
           email = {this.state.email}
           country = {this.state.country}
+          handleChange = {this.handleChange}
         />
       </>
     )
